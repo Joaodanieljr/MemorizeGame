43f3520700c490fae0023dc5e559467a865f254d
Joao Junior <Joaodanieljr@gmail.com>
2021-03-26 23:07:33 -0300

    Start Project and view

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Joao Daniel Pereira Junior on 25/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .padding()
+            .foregroundColor(.orange)
+            .font(.largeTitle)
+    }
+}
+
+struct CardView:View {
+    var isFaceUp:Bool
+    var body: some View{
+        ZStack {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+            
+            
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
